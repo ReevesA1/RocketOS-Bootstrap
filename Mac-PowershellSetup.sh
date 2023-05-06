3f7110fae202ebb216c6452c993401108f78323b
ReevesA1 <adamrivard87@gmail.com>
2023-05-06 09:01:38 -0400

    d

--- Mac-PowershellSetup.sh
+++ Mac-PowershellSetup.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
 
-write-host "test1"
--- Universal-Main-Bootstrap-Script.ps1
+++ Universal-Main-Bootstrap-Script.ps1
@@ -1,15 +1,19 @@
 if ($IsWindows) {
   # Install Choco and Winget, ETC!!!!
   # Install Git and Gum, ETC !!!
+  write-host "test windows"
 }
 
 elseif ($IsMacOS) {
   # Install Homebrew and Nix, ETC!!!
   # Install Git and Gum, ETC !!! 
+  write-host "test mac"
 }
 
 elseif ($IsLinux) {
   # Detect Distro Ubuntu, Fedora, Arch, NixOS
   # Install flatpaks,Snaps,Nix ETC!!!!
   # Install Git and Gum, ETC !!!
+
+  write-host "test linux"
 }
