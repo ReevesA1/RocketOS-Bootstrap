d20c148570f24caf8c480615372c651c58429459
ReevesA1 <adamrivard87@gmail.com>
2023-05-06 10:48:27 -0400

    d

--- Bootstrap-Mac.sh
+++ Bootstrap-Mac.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
 
 
-  #? Example of calling a script 
-  #* Invoke-Expression "$HOME/Downloads/RocketOS-Bootstrap/Mac-PowershellSetup.sh"
-  #TODO Install Package Managers: Homebrew and Nix, ETC!!!
-  #* Check App MasterList for all package managers!!! 
-  #* Have a script for each package manager!!!!
-  #TODO Install Apps: POWERSHELL, Git and Gum, ETC !!! 
+#? Example of calling a script 
+#* Invoke-Expression "$HOME/Downloads/RocketOS-Bootstrap/Mac-PowershellSetup.sh"
+#TODO Install Package Managers: Homebrew and Nix, ETC!!!
+#* Check App MasterList for all package managers!!! 
+#* Have a script for each package manager!!!!
+#TODO Install Apps: POWERSHELL, Git and Gum, ETC !!! 
 
 
-  #! Package Managers
+#! --------Package Managers------------
   
-  #Nix
-  $HOME/Downloads/RocketOS-Bootstrap/PackageManagers/Nix-Bash.sh
+#Nix
+$HOME/Downloads/RocketOS-Bootstrap/PackageManagers/Nix-Bash.sh
+
+#! ----------Apps----------
+
+# Nix Apps
+if command -v nix-env >/dev/null; then
+
+    # Channels
+    nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
+    nix-channel --update
+
+    # Apps
+    nix-env -iA nixpkgs.powershell
+
+else
+    echo "Nix is not installed"
+fi
