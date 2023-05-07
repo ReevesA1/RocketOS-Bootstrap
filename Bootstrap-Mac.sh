7ff97adfd6667a895ff8e9c0950d332c68a1a31a
ReevesA1 <adamrivard87@gmail.com>
2023-05-07 19:47:33 -0400

    d

--- Bootstrap-Mac.sh
+++ Bootstrap-Mac.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/zsh
 
 #? Example of calling a script
 #* Invoke-Expression "$HOME/Downloads/RocketOS-Bootstrap/Mac-PowershellSetup.sh"
--- Powershell/Mac-Powershell.sh
+++ Powershell/Mac-Powershell.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/zsh
 
 brew install --cask powershell # (Installs here /usr/local/microsoft/powershell/7/pwsh)
 brew install --cask powershell-preview  # from cask-versions Tap (installs here /usr/local/microsoft/powershell/7-preview/pwsh)
