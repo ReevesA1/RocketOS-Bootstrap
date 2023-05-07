ca7ed8e768ddfc3a5245044df7295575018bcd2f
ReevesA1 <adamrivard87@gmail.com>
2023-05-07 10:20:15 -0400

    d

--- Bootstrap-Linux.sh
+++ Bootstrap-Linux.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-#? Example of calling a script 
+#? Example of calling a script
 #* Invoke-Expression "$HOME/Downloads/RocketOS-Bootstrap/Mac-PowershellSetup.sh"
 #TODO Detect Distro Ubuntu, Fedora, Arch, NixOS
-#* Check App MasterList for all package managers!!! 
+#* Check App MasterList for all package managers!!!
 #* Have a script for each package manager!!!!
 #TODO Install Package managers according to distro: flatpaks,Snaps,Nix ETC!!!!
 #* Nix will have two ways!!!!!!!!!!!!!
--- Bootstrap-Mac.sh
+++ Bootstrap-Mac.sh
@@ -1,57 +1,40 @@
 #!/usr/bin/env bash
 
-
-#? Example of calling a script 
+#? Example of calling a script
 #* Invoke-Expression "$HOME/Downloads/RocketOS-Bootstrap/Mac-PowershellSetup.sh"
 #TODO Install Package Managers: Homebrew and Nix, ETC!!!
-#* Check App MasterList for all package managers!!! 
+#* Check App MasterList for all package managers!!!
 #* Have a script for each package manager!!!!
-#TODO Install Apps: POWERSHELL, Git and Gum, ohmyzsh, ETC !!! 
+#TODO Install Apps: POWERSHELL
+#* powershell
+#* mas
+#* gum
+#* alacritty
+#* jetbrains-mono
+#* starship
+#* oh-my-zsh
 #TODO Copy Notion MAc bootstrap Template shit in here
-#* started I added xcode already 
-#TODO make me choose between homebrew and nix right off the bat or install both? 
-#TODO make nix and homebrew methods both make powershell my default shell 
+#* started I added xcode already
+#TODO make me choose between homebrew and nix right off the bat or install both?
+#TODO make nix and homebrew methods both make powershell my default shell
 #TODO install icon powershell module and install homebrew
-
 
 #! --------Disable Mac gate keeper-----------
 
 sudo spctl --master-disable
 
-
 #! --------Install Xcode-----------
 
 xcode-select --install
 
-
 #! --------Package Managers------------
 
 #Nix
-$HOME/Downloads/RocketOS-Bootstrap/PackageManagers/Nix-Bash.sh
-$HOME/Downloads/RocketOS-Bootstrap/PackageManagers/Homebrew-Bash.sh
+$HOME/Downloads/RocketOS-Bootstrap/PackageManagers/Nix.sh
+$HOME/Downloads/RocketOS-Bootstrap/PackageManagers/Homebrew.sh
 
 #! ----------Apps (Only to get RocketOS Going)---------
-
-
-if command -v nix-env >/dev/null; then
-    # Channels
-    nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
-    nix-channel --update
-
-    # Apps (no need for nix-env -iA nixpkgs-unstable.packageName)
-    nix-env -iA nixpkgs.powershell
-    nix-env -iA nixpkgs.mas
-    nix-env -iA nixpkgs.gum
-    nix-env -iA nixpkgs.alacritty
-    nix-env -iA nixpkgs.jetbrains-mono
-    nix-env -iA nixpkgs.starship
-
-    # Append to Powershell $profile so it can see nix apps
-    mkdir -p $HOME/.config/powershell && echo '`n$env:PATH += ":/nix/var/nix/profiles/default/bin/"' >> $HOME/.config/powershell/profile.ps1 
-
-
-
-
+#! I will use Homebrew for this, I tried using nix but it gets complicated. Just use nix for one of apps not for the base setup
 
 echo "You should now be able to launch RocketOS from the repo readme!"
 read -p "Press Enter to continue"
