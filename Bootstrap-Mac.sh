6ccd5ca415773550072c30c12cfb6b46d5b5cf6e
ReevesA1 <adamrivard87@gmail.com>
2023-05-07 09:34:07 -0400

    s

--- Bootstrap-Mac.sh
+++ Bootstrap-Mac.sh
@@ -11,11 +11,18 @@
 #* started I added xcode already 
 #TODO make me choose between homebrew and nix right off the bat or install both? 
 #TODO make nix and homebrew methods both make powershell my default shell 
+#TODO install icon powershell module and install homebrew
+
+
+#! --------Disable Mac gate keeper-----------
+
+sudo spctl --master-disable
 
 
 #! --------Install Xcode-----------
 
 xcode-select --install
+
 
 #! --------Package Managers------------
   
@@ -25,20 +32,7 @@ $HOME/Downloads/RocketOS-Bootstrap/PackageManagers/Homebrew-Bash.sh
 
 #! ----------Apps (Only to get RocketOS Going)---------
 
-echo "Please choose an option:"
-echo "1) Homebrew as Base"
-echo "2) Nix as Base"
-read -p "Enter your choice (1 or 2): " choice
 
-# HOMEBREW Base
-if [[ $choice == "1" ]]; then
-    echo "You selected Homebrew as Base"
-
-
-
-# NIX Base
-elif [[ $choice == "2" ]]; then
-    echo "You selected Nix as Base"
     if command -v nix-env >/dev/null; then
         # Channels
         nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
@@ -58,13 +52,6 @@ elif [[ $choice == "2" ]]; then
 
 
 
-    else
-        echo "Nix is not installed or I must source the terminal"
-    fi
-
 
     echo "You should now be able to launch RocketOS from the repo readme!"
     read -p "Press Enter to continue"
-else
-    echo "Invalid choice"
-fi
