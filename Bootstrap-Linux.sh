ad5abc3bfefb002219148a824a7386e7add0dc4d
ReevesA1 <adamrivard87@gmail.com>
2023-05-13 11:05:02 -0400

    d

--- Bootstrap-Linux.sh
+++ Bootstrap-Linux.sh
@@ -1,9 +1,11 @@
 #!/bin/zsh
 #Using this shbang for echo and read with color to work
 
-
-
 #! --------Package Managers------------
+
+#? Linux Bootstrap Flatpak & Snaps get going
+#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+#snap ill have to start socket?
 
 #! ----------Install Powershell with modules---------
 
