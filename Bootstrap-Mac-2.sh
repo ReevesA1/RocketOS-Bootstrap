90d1c0f5e8e281e70322c484ad177ce75a1c00bb
ReevesA1 <adamrivard87@gmail.com>
2023-05-13 16:20:36 -0400

    f

--- Bootstrap-Mac-2.sh
+++ Bootstrap-Mac-2.sh
@@ -36,10 +36,10 @@ brew tap homebrew/core
 echo "\033[32mFinish Nix Install... Press Enter to continue  \033[0m"
 read
 
-# Add Unstable Channel and use the unstable prefix to install apps like so `nix-env -iA unstable.gum`
+#? Add Unstable Channel and use the unstable prefix to install apps like so `nix-env -iA unstable.gum`
+#? I could actualy name it anything by changing the name unstable. The last word in the next command !!!
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
 nix-channel --update
-
 
 #! Install Apps to get rocketOS Going
 #########################
