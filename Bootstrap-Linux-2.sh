afb2b2d317a53025f67ea36c55e1af891933be74
ReevesA1 <adamrivard87@gmail.com>
2023-05-27 14:11:53 -0400

    s

---
+++ Bootstrap-Linux-2.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+#Using this shbang for echo and read with color to work
+
+
+#! Install Apps to get rocketOS Going
+#########################
+######  APPS  ###########
+#########################
---
+++ Bootstrap-Windows-2.ps1
@@ -0,0 +1,7 @@
+#! Install Apps to get rocketOS Going
+#########################
+######  APPS  ###########
+#########################
+
+scoop install charm-gum
+winget install -e --id Alacritty.Alacritty
