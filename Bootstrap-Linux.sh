f63941f6107b6514746ad19352717f50a6eb7bfb
ReevesA1 <adamrivard87@gmail.com>
2023-05-27 09:02:37 -0400

    d

--- Bootstrap-Linux.sh
+++ Bootstrap-Linux.sh
@@ -10,3 +10,19 @@
 #! ----------Install Powershell with modules---------
 
 #! ----------Apps (Only to get RocketOS Going Properly)---------
+
+#!SORT
+- Fonts $(sudo dnf install jetbrains-mono-fonts.noarch) then I have to manualy choose fonts in some terminals
+- $(sudo apt install conky-all)
+- $(nix-env -iA nixpkgs.gum)
+- $(nix-env -iA nixpkgs.alacritty)
+- why not snaps???? $(sudo snap install alacritty --classic)
+- Ubuntu
+- $(sudo apt update && sudo apt upgrade)
+- $(sudo add-apt-repository ppa:aslatter/ppa -y)
+- $(sudo apt install alacritty)
+- if Gnome
+- $(flatpak install flathub ca.desrt.dconf-editor)
+- $(flatpak install flathub com.mattjakeman.ExtensionManager)
+
+nix-env -iA nixpkgs.lsb-release #instead of #sudo apt install lsb-release #paru lsb-release #sudo dnf install redhat-lsb-core
