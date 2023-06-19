d5e71c1dad6a3cd9f6b7813ba90e8dfba0dcb2fb
ReevesA1 <adamrivard87@gmail.com>
2023-06-19 19:53:42 -0400

    s

--- NixOS/nix-configs.nix
+++ NixOS/nix-configs.nix
@@ -221,9 +221,9 @@
 
   # FLATPAKS
   services.flatpak.enable = true;
-  services.dbus.enable = true;
 
   # FlatPaks Dependencies
+  services.dbus.enable = true; # needed for flatpaks to talk to each other
   xdg.portal = {
     enable = true; # This for sure needs to be on
     #wlr.enable = true; # Not sure what this setting is for
