5ae5927d49a5a18af9a6ff195e2e29f37251c9e5
ReevesA1 <adamrivard87@gmail.com>
2023-06-24 13:48:04 -0400

    s

--- NixOS/ROG-Laptop-configs.nix
+++ NixOS/ROG-Laptop-configs.nix
@@ -433,24 +433,41 @@
   #     Remove Bloat    #
   #######################
 
-environment.gnome.excludePackages = with pkgs; [
-  #gnome-tour
-  #gnome-photos
-] ++ (with pkgs.gnome; [
-  geary
-  totem
-  cheese
-  epiphany
-  gnome-contacts
-  gnome-weather
-  gnome-clocks
-  gnome-maps
-  gnome-calculator
+#environment.gnome.excludePackages = with pkgs; [
+#  gnome-tour
+#  gnome-photos
+#] ++ (with pkgs.gnome; [
+#  geary
+#  totem
+#  cheese
+#  epiphany
+#  gnome-contacts
+#  gnome-weather
+#  gnome-clocks
+#  gnome-maps
+#  gnome-calculator
+#  gnome-terminal
+#  gnome-connections
+#]);
+
+environment.gnome.excludePackages = (with pkgs; [
+  gnome-photos
+  gnome-tour
+]) ++ (with pkgs.gnome; [
+  cheese # webcam tool
+  gnome-music
   gnome-terminal
-  gnome-connections
+  gedit # text editor
+  epiphany # web browser
+  geary # email reader
+  evince # document viewer
+  gnome-characters
+  totem # video player
+  tali # poker game
+  iagno # go game
+  hitori # sudoku game
+  atomix # puzzle game
 ]);
-
-
 
   ##########################
   ##  Insecure Packages   ##
