bd764d976a5fd0b631a73cf1c381fa8541e0591b
ReevesA1 <adamrivard87@gmail.com>
2023-06-23 22:32:16 -0400

    d

--- Shared-Code/JetBrainsMonoFont-MacLinux.sh
+++ Shared-Code/JetBrainsMonoFont-MacLinux.sh
@@ -12,27 +12,37 @@
 # Set the download URL
 Jetbrainurl="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.0/JetBrainsMono.zip"
 
-# Download the font
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  # On macOS
+if [ ! -f JetBrainsMono.zip ]; then
+
+  #Download
   curl -L -O $Jetbrainurl
-else
-  # On Linux
-  if [ ! -f JetBrainsMono.zip ]; then #I could do the same for mac
-    curl -L -O $Jetbrainurl
+
+  # Unzip the downloaded file into the fonts directory
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    # On macOS
+    unzip -j JetBrainsMono.zip -d ~/Library/Fonts/
   else
-    echo "JetbrainsMono.zip exists"
+    # On Linux
+    if [ ! -d ~/.local/share/fonts/ ]; then
+      mkdir -p ~/.local/share/fonts/
+    fi
+    unzip -j JetBrainsMono.zip -d ~/.local/share/fonts/
   fi
+
+else
+  echo "JetbrainsMono.zip exists"
 fi
 
-# Unzip the downloaded file into the fonts directory
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  # On macOS
-  unzip -j JetBrainsMono.zip -d ~/Library/Fonts/
-else
-  # On Linux
-  if [ ! -d ~/.local/share/fonts/ ]; then
-    mkdir -p ~/.local/share/fonts/
-  fi
-  unzip -j JetBrainsMono.zip -d ~/.local/share/fonts/
-fi
+#! I don't think I need to seperate mac and linux
+# Download the font
+#if [[ "$OSTYPE" == "darwin"* ]]; then
+#  # On macOS
+#  curl -L -O $Jetbrainurl
+#else
+#  # On Linux
+#  if [ ! -f JetBrainsMono.zip ]; then #I could do the same for mac
+#    curl -L -O $Jetbrainurl
+#  else
+#    echo "JetbrainsMono.zip exists"
+#  fi
+#fi
