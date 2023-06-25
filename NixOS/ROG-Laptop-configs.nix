9b7a5cf5b1d9511f600851e93e6d574bdde0348a
ReevesA1 <adamrivard87@gmail.com>
2023-06-25 12:38:34 -0400

    s

--- NixOS/ROG-Laptop-configs.nix
+++ NixOS/ROG-Laptop-configs.nix
@@ -368,9 +368,9 @@
     enable = true;
     description = "Start Ulauncher";
     script = "${pkgs.ulauncher}/bin/ulauncher --hide-window";
-
-    documentation = [ "https://github.com/Ulauncher/Ulauncher/blob/f0905b9a9cabb342f9c29d0e9efd3ba4d0fa456e/contrib/systemd/ulauncher.service" ];
-    wantedBy = [ "graphical.target" "multi-user.target" ];
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
   };
 
 #!No GO auto starting proton
