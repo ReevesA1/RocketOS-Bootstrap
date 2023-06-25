7aa7f07924bb6e0f123c40c9f2dee866e12f9dd8
ReevesA1 <adamrivard87@gmail.com>
2023-06-25 12:21:58 -0400

    s

--- NixOS/ROG-Laptop-configs.nix
+++ NixOS/ROG-Laptop-configs.nix
@@ -362,6 +362,17 @@
       DefaultTimeoutStopSec=10s
     '';
 
+#! Ulauncher start at boot
+  systemd.user.services.ulauncher = {
+    enable = true;
+    description = "Start Ulauncher";
+    script = "${pkgs.ulauncher}/bin/ulauncher --hide-window";
+
+    documentation = [ "https://github.com/Ulauncher/Ulauncher/blob/f0905b9a9cabb342f9c29d0e9efd3ba4d0fa456e/contrib/systemd/ulauncher.service" ];
+    wantedBy = [ "graphical.target" "multi-user.target" ];
+    after = [ "display-manager.service" ];
+  };
+
 #!No GO auto starting proton
 /*
   user.services.protonvpn = {
