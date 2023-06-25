581b7e0f68f76ee855a3d6512d105e9570e8eb91
ReevesA1 <adamrivard87@gmail.com>
2023-06-25 13:20:01 -0400

    d

--- NixOS/ROG-Laptop-configs.nix
+++ NixOS/ROG-Laptop-configs.nix
@@ -388,6 +388,17 @@ user.services.protonvpn-cli = {
   };
 };
 
+#! Synergy start at boot
+#see if it worked with systemctl --user status ulauncher and systemctl --user start ulauncher to start it
+  user.services.synergy = {
+    enable = true;
+    description = "Start Synergy";
+    script = "com.symless.synergy --hide-window";
+    #/var/lib/flatpak/exports/bin/com.symless.synergy
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+  };
 }; 
 
   ########################
