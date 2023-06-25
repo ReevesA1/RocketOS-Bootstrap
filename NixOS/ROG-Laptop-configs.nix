3b68f44efa8abb6b6a3ee0256522fc4a007f09a2
ReevesA1 <adamrivard87@gmail.com>
2023-06-25 11:41:37 -0400

    s

--- NixOS/ROG-Laptop-configs.nix
+++ NixOS/ROG-Laptop-configs.nix
@@ -51,6 +51,8 @@
   #! Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  security.pam.services.gdm.enableGnomeKeyring = true;
+
 
   #! Wayland Support
   #Original line that works
