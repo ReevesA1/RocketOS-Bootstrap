63d29c43328137b9490eb54828729698479dd8e9
ReevesA1 <adamrivard87@gmail.com>
2023-06-25 15:02:09 -0400

    s

--- NixOS/ROG-Laptop-configs.nix
+++ NixOS/ROG-Laptop-configs.nix
@@ -410,9 +410,11 @@ user.services.synergy = {
   after = [ "graphical-session.target" ];
   serviceConfig = {
     ExecStartPre = "${pkgs.bash}/bin/bash -c 'nohup ${pkgs.flatpak}/bin/flatpak run com.symless.synergy";
+    ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
     ExecStart = "${pkgs.wmctrl}/bin/wmctrl -c Synergy'";
   };
 };
+
 
 
 }; 
