54e93afc3d2ae757597cd15ebeecba053c290f0c
ReevesA1 <adamrivard87@gmail.com>
2023-06-26 19:25:06 -0400

    d

--- NixOS/ROG-Laptop-configs.nix
+++ NixOS/ROG-Laptop-configs.nix
@@ -303,6 +303,7 @@
   ### Testing Add to all other ones if yes###
   ##########################################
   inetutils #needed for telnet good to have
+  killall #used to kill conky in protonvpn-cli service
 
   ];
 
@@ -394,7 +395,7 @@ user.services.protonvpn-cli = {
   after = [ "suspend.target" "graphical-session.target" ];
   serviceConfig = {
     ExecStartPre = "${pkgs.coreutils}/bin/sleep 10";
-    ExecStart = "${pkgs.bash}/bin/bash -c 'while true; do if ${pkgs.protonvpn-cli}/bin/protonvpn-cli status | ${pkgs.gnugrep}/bin/grep -q \"No active Proton VPN connection.\"; then ${pkgs.protonvpn-cli}/bin/protonvpn-cli killswitch --off && ${pkgs.protonvpn-cli}/bin/protonvpn-cli killswitch --on && ${pkgs.protonvpn-cli}/bin/protonvpn-cli connect --cc CA && ${pkgs.kill}/bin/kill ${pkgs.conky}/bin/conky && ${pkgs.conky}/bin/conky; fi; sleep 30; done'";
+    ExecStart = "${pkgs.bash}/bin/bash -c 'while true; do if ${pkgs.protonvpn-cli}/bin/protonvpn-cli status | ${pkgs.gnugrep}/bin/grep -q \"No active Proton VPN connection.\"; then ${pkgs.protonvpn-cli}/bin/protonvpn-cli killswitch --off && ${pkgs.protonvpn-cli}/bin/protonvpn-cli killswitch --on && ${pkgs.protonvpn-cli}/bin/protonvpn-cli connect --cc CA && ${pkgs.killall}/bin/killall conky && ${pkgs.conky}/bin/conky; fi; sleep 30; done'";
     Restart = "always";
   };
 };
