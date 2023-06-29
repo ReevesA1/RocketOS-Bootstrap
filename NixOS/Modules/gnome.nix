e6a1ce335748703225a6a3a0b153d3244b63d075
ReevesA1 <adamrivard87@gmail.com>
2023-06-29 12:47:56 -0400

    d

---
+++ NixOS/Modules/gnome.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    # GNOME applications
+    gnome3.gedit
+    gnome3.evince
+    gnome3.eog
+  ];
+}
