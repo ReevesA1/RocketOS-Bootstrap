efd2c60b8313c9b08f8822898066f74c7977aa5f
ReevesA1 <adamrivard87@gmail.com>
2023-06-29 13:13:05 -0400

    s

--- NixOS/Parallels-configs.nix
+++ NixOS/Parallels-configs.nix
@@ -138,6 +138,10 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  
+  # Allow Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Allow Unstable Packages
   # Make sure to run these to commands first 
   # sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
