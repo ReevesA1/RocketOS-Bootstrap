a50f00b8a23f18c81c394df185e70861b9570b6d
ReevesA1 <adamrivard87@gmail.com>
2023-07-01 08:21:22 -0400

    s

---
+++ NixOS/Modules/systemd-services.nix
@@ -0,0 +1,10 @@
+#? Diagnose with these next commands
+#journalctl --user-unit foo.service  
+#systemctl --user status foo 
+#systemctl --user start foo
+
+{ config, pkgs, lib, ... }:
+  {
+
+
+  }
--- NixOS/ROG-Laptop-configs.nix
+++ NixOS/ROG-Laptop-configs.nix
@@ -216,26 +216,7 @@
 #journalctl --user-unit foo.service  
 #systemctl --user status foo 
 #systemctl --user start foo
-  #SystemD Services
 
-  #! Pole Kit from Chirs Titus
-  systemd = {
-  user.services.polkit-gnome-authentication-agent-1 = {
-    description = "polkit-gnome-authentication-agent-1";
-    wantedBy = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-  };
-    extraConfig = ''
-      DefaultTimeoutStopSec=10s
-    '';
 
 
 };
