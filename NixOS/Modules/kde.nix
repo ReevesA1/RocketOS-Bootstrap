6dd655c0603c0d43d1c9b7fe84c79adb5eefd3e9
ReevesA1 <adamrivard87@gmail.com>
2023-07-01 13:35:40 -0400

    s

--- NixOS/Modules/kde.nix
+++ NixOS/Modules/kde.nix
@@ -10,12 +10,15 @@
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
+#!Excluding some KDE Plasma applications from the default install
+environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+  discover
+];
+
+#! Install Packages 
   environment.systemPackages = with pkgs; [
     # KDE applications
-    kate
-    dolphin
-    okular
-  ];
-}
 
-#!Excluding some KDE Plasma applications from the default install
+  ];
+
+}
