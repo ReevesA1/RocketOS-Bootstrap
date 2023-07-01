1725adf33c2a4fe0358c95561eef660cc9b34474
ReevesA1 <adamrivard87@gmail.com>
2023-07-01 16:03:23 -0400

    d

--- NixOS/Parallels-configs.nix
+++ NixOS/Parallels-configs.nix
@@ -54,7 +54,7 @@ networking.hostName = "M2-Parallels-NixOS";
 
 #! Wayland Support
 #Gnome Wayland
-services.xserver.displayManager.gdm.wayland = false;  # wayland is better for parallels but not virtualbox also wayland no good for barrier or synergy yet!!!
+services.xserver.displayManager.gdm.wayland = true;  # wayland is better for parallels but not virtualbox also wayland no good for barrier or synergy yet!!!
 #KDE Wayland
 #services.xserver.displayManager.defaultSession = "plasmawayland"; # wayland is better for parallels but not virtualbox also wayland no good for barrier or synergy yet!!!
 
