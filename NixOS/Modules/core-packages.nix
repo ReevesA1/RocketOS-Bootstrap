fb083455cf60d21bb959f4b9b298fdc8e4048e99
ReevesA1 <adamrivard87@gmail.com>
2023-07-02 10:20:12 -0400

    s

--- NixOS/Modules/core-packages.nix
+++ NixOS/Modules/core-packages.nix
@@ -101,6 +101,39 @@
 
   
 
+
+  ##################
+  ### TESTING    ###
+  ##################
+    chromium = {
+      enable = true;
+      extensions = [
+        "cfhdojbkjhnklbpkdaibdccddilifddb" # Adblock Plus
+        "ejddcgojdblidajhngkogefpkknnebdh" # AutoplayStopper
+        "ajopnjidmegmdimjlfnijceegpefgped" # BetterTTV
+        "nomnklagbgmgghhjidfhnoelnjfndfpd" # Canvas Blocker
+        "gphhapmejobijbbhgpjhcjognlahblep" # GNOME Shell integration
+        "mlboohjioameadaedfjcpemcaangkkbp" # Linux Scroll Speed Fix
+        "pkehgijcmpdhfbdbbnkijodmdjhbjlgp" # Privacy Badger
+        "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
+      ];
+      extraOpts = {
+        "AutoplayAllowed" = false;
+        "BrowserSignin" = 0;
+        "PasswordManagerEnabled" = false;
+        "SpellcheckEnabled" = true;
+        "SpellcheckLanguage" = [
+          "de"
+          "en-NZ"
+          "fr-FR"
+          "nb-NO"
+        ];
+        "SyncDisabled" = true;
+      };
+    };
+
+
+
   ];
 
 #! services needed for some apps
@@ -109,6 +142,7 @@
 services.locate.enable = true;
 users.groups.plocate = {};
 services.locate.interval = "hourly";
+
 
 
 
