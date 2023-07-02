04d4a2b6631509cb29bed128afc2326d359fe315
ReevesA1 <adamrivard87@gmail.com>
2023-07-02 19:31:22 -0400

    s

--- NixOS/Modules/cron.nix
+++ NixOS/Modules/cron.nix
@@ -7,7 +7,7 @@
 services.cron = {
   #enable = true;#gets enabled in core-configs.nix
   systemCronJobs = [
-    "0 7,19 * * * flatpak update && flatpak uninstall --unused"
+    "0 7,19 * * * flatpak update --noninteractive --assumeyes && flatpak uninstall --unused"
     #?0 8 * * * /path/to/example.sh && curl -fsS --retry 3 https://hc-ping.com/your-unique-check-url
   ];
 };
