916fdf3913cbab3cf2e9c49e577417375e2251ad
ReevesA1 <adamrivard87@gmail.com>
2023-07-03 13:29:48 -0400

    s

--- NixOS/Ser3-configs.nix
+++ NixOS/Ser3-configs.nix
@@ -89,7 +89,7 @@ powerManagement.resumeCommands = ''
     };
     services."conky-stop-multiple" = {
       script = ''
-      ${pkgs.powershell}/bin/pwsh $HOME/MEGAsync/Scripts/SystemD-Timers/SER3/conky-stop-multiple.ps1
+      ${pkgs.powershell}/bin/pwsh $HOME/MEGAsync/Scripts/SystemD-Timers/SER3/conky-stop-multiple.sh
       '';
       serviceConfig = {
         User = "rocket";
