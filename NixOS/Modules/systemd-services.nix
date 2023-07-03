f0d4b5e5fe1df02d9f03638aa6e8f64ffecdfba5
ReevesA1 <adamrivard87@gmail.com>
2023-07-03 19:59:07 -0400

    s

--- NixOS/Modules/systemd-services.nix
+++ NixOS/Modules/systemd-services.nix
@@ -70,7 +70,7 @@
         ${pkgs.powershell}/bin/pwsh $HOME/MEGAsync/Scripts/SystemD-Timers/Universal/daily-universal.ps1
       '';
       serviceConfig = {
-        User = "root";
+        User = "rocket";
         Environment = "PATH=${pkgs.flatpak}/bin:$HOME/.local/share:/run/current-system:/var/lib";
       };
     };
