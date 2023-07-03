bfdf45cae541d4eb07101e1559eaa364b06dbb80
ReevesA1 <adamrivard87@gmail.com>
2023-07-03 07:59:01 -0400

    d

--- NixOS/Modules/cron.nix
+++ NixOS/Modules/cron.nix
@@ -1,6 +1,11 @@
 #! Diagnose with these next commands
 #journalctl -u cron -n 20
 #systemctl status cron
+#journalctl -u cron -g CMD
+#journalctl -u cron -g CMD --since "2023-07-01 19:00:00" --until "2023-07-02 7:00:00"
+#journalctl -u cron -g CMD --since "2 hours ago"
+
+
 
 
 { config, pkgs, lib, ... }:
