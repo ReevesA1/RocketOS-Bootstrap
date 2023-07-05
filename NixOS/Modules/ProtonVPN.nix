98b37b8989889a5dd0384dcecf255f412c95a79e
ReevesA1 <adamrivard87@gmail.com>
2023-07-05 16:45:44 -0400

    s

--- NixOS/Modules/ProtonVPN.nix
+++ NixOS/Modules/ProtonVPN.nix
@@ -28,10 +28,11 @@ systemd  = {
       '';
       serviceConfig = {
         User = "rocket";
-        Environment = [
-          "PATH=${pkgs.flatpak}/bin ${pkgs.protonvpn-cli}/bin ${pkgs.powershell}/bin ${pkgs.dbus}/bin ${pkgs.bash}/bin ${pkgs.starship}/bin"
-          "XDG_CURRENT_DESKTOP=GNOME"
-        ];
+        Environment = ''
+        PATH=${pkgs.flatpak}/bin:$HOME/.local/share:/run/current-system:/var/lib:${pkgs.protonvpn-cli}/bin/:${pkgs.powershell}/bin/:${pkgs.dbus}/bin/ ${pkgs.bash}/bin ${pkgs.starship}/bin
+        DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+        XDG_CURRENT_DESKTOP=GNOME
+        '';   
       };
     };
   };
