2fc1dd7b24eec391e1520c622478aa3b19a26bba
ReevesA1 <adamrivard87@gmail.com>
2023-07-05 19:13:46 -0400

    s

--- NixOS/Modules/ProtonVPN.nix
+++ NixOS/Modules/ProtonVPN.nix
@@ -43,32 +43,32 @@ services.protonvpn-resume = {
 }
 
 
-/*
-#! RECONNECT Timer ONLY WORKS ON GNOME
-#! DOES NOT WORK ON KDE - I could not figure out how to export this dynamicaly "dbus-launch --auto-syntax"
-systemd  = {
-    
-    #? Monitor Internet connection
-    timers."protonvpn-cli_connection_monitor" = {
-      wantedBy = [ "timers.target" ];
-      timerConfig = {
-        OnCalendar = "*-*-* *:*:00"; #set to everyminute is it to much?
-        Unit = "protonvpn-cli_connection_monitor.service";
-      };
-    };
 
-    #!Powershell Version Works only for Gnome
-    services."protonvpn-cli_connection_monitor" = {
-      script = ''
-        ${pkgs.powershell}/bin/pwsh $HOME/MEGAsync/Scripts/SystemD-Timers/Universal/protonvpn-cli_connection_monitor.ps1
-      '';
-      serviceConfig = {
-        User = "rocket";
-        Environment = "PATH=${pkgs.flatpak}/bin:${pkgs.protonvpn-cli}/bin/:${pkgs.powershell}/bin/:${pkgs.bash}/bin:${pkgs.starship}/bin:${pkgs.git}/bin:${pkgs.curl}/bin:${pkgs.coreutils}/bin";
-      };
-    };
-  };
-}
+##! RECONNECT Timer ONLY WORKS ON GNOME
+##! DOES NOT WORK ON KDE - I could not figure out how to export this dynamicaly "dbus-launch --auto-syntax"
+#systemd  = {
+#    
+#    #? Monitor Internet connection
+#    timers."protonvpn-cli_connection_monitor" = {
+#      wantedBy = [ "timers.target" ];
+#      timerConfig = {
+#        OnCalendar = "*-*-* *:*:00"; #set to everyminute is it to much?
+#        Unit = "protonvpn-cli_connection_monitor.service";
+#      };
+#    };
+#
+#    #!Powershell Version Works only for Gnome
+#    services."protonvpn-cli_connection_monitor" = {
+#      script = ''
+#        ${pkgs.powershell}/bin/pwsh $HOME/MEGAsync/Scripts/SystemD-Timers/Universal/protonvpn-cli_connection_monitor.ps1
+#      '';
+#      serviceConfig = {
+#        User = "rocket";
+#        Environment = "PATH=${pkgs.flatpak}/bin:${pkgs.protonvpn-cli}/bin/:${pkgs.powershell}/bin/:${pkgs.bash}/bin:${pkgs.starship}/bin:${pkgs.git}/bin:${pkgs.curl}/bin:${pkgs.coreutils}/bin";
+#      };
+#    };
+#  };
+#}
 
 
 
