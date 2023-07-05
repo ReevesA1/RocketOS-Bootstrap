cfa4c13896fcaa635f58302b6a5bcbd0018e4124
ReevesA1 <adamrivard87@gmail.com>
2023-07-05 19:43:37 -0400

    d

--- NixOS/Modules/ProtonVPN.nix
+++ NixOS/Modules/ProtonVPN.nix
@@ -13,65 +13,32 @@
 
 
 
-#!Connect at boot
-systemd.services.protonvpn-boot = {
-  script = ''
-    ${pkgs.protonvpn-cli}/bin/protonvpn-cli connect -f
-  '';
-  serviceConfig = {
-    Type = "simple";
-    ExecStop = "${pkgs.protonvpn-cli}/bin/protonvpn-cli disconnect";
-    ExecReload = "${pkgs.protonvpn-cli}/bin/protonvpn-cli reconnect";
-    RemainAfterExit = "yes";
-    Restart = "on-failure";
-    RestartSec = 5;
-    StartLimitInterval = 500;
-    StartLimitBurst = 5;
-    User = "rocket";
-  };
-};
-
-#!Reconnect after suspend
-systemd.services.protonvpn-resume = {
-  script = ''
-    ${pkgs.protonvpn-cli}/bin/protonvpn-cli reconnect
-  '';
-  serviceConfig = {
-    Type = "oneshot";
-    User = "rocket";
-  };
-};
-
-
-}
-
 
 
 ##! RECONNECT Timer ONLY WORKS ON GNOME
 ##! DOES NOT WORK ON KDE - I could not figure out how to export this dynamicaly "dbus-launch --auto-syntax"
-#systemd  = {
-#    
-#    #? Monitor Internet connection
-#    timers."protonvpn-cli_connection_monitor" = {
-#      wantedBy = [ "timers.target" ];
-#      timerConfig = {
-#        OnCalendar = "*-*-* *:*:00"; #set to everyminute is it to much?
-#        Unit = "protonvpn-cli_connection_monitor.service";
-#      };
-#    };
-#
-#    #!Powershell Version Works only for Gnome
-#    services."protonvpn-cli_connection_monitor" = {
-#      script = ''
-#        ${pkgs.powershell}/bin/pwsh $HOME/MEGAsync/Scripts/SystemD-Timers/Universal/protonvpn-cli_connection_monitor.ps1
-#      '';
-#      serviceConfig = {
-#        User = "rocket";
-#        Environment = "PATH=${pkgs.flatpak}/bin:${pkgs.protonvpn-cli}/bin/:${pkgs.powershell}/bin/:${pkgs.bash}/bin:${pkgs.starship}/bin:${pkgs.git}/bin:${pkgs.curl}/bin:${pkgs.coreutils}/bin";
-#      };
-#    };
-#  };
-#}
+systemd  = {
+    
+    #? Monitor Internet connection
+    timers."protonvpn-cli_connection_monitor" = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "*-*-* *:*:00"; #set to everyminute is it to much?
+        Unit = "protonvpn-cli_connection_monitor.service";
+      };
+    };
+
+    services."protonvpn-cli_connection_monitor" = {
+      script = ''
+        ${pkgs.powershell}/bin/pwsh $HOME/MEGAsync/Scripts/SystemD-Timers/Universal/protonvpn-cli_connection_monitor.ps1
+      '';
+      serviceConfig = {
+        User = "rocket";
+        Environment = "PATH=${pkgs.flatpak}/bin:${pkgs.protonvpn-cli}/bin/:${pkgs.powershell}/bin/:${pkgs.bash}/bin:${pkgs.starship}/bin:${pkgs.git}/bin:${pkgs.curl}/bin:${pkgs.coreutils}/bin";
+      };
+    };
+  };
+}
 
 
 
