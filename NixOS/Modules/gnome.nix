a8bd7f94a8224e8fb6c0d583a925b26bbaf011e0
ReevesA1 <adamrivard87@gmail.com>
2023-07-06 18:03:28 -0400

    d

--- NixOS/Modules/gnome.nix
+++ NixOS/Modules/gnome.nix
@@ -81,11 +81,20 @@ environment.systemPackages = with pkgs; [
 
 
 
-  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
-  pkgs.gst_all_1.gst-plugins-good
-  pkgs.gst_all_1.gst-plugins-bad
-  pkgs.gst_all_1.gst-plugins-ugly
-  #pkgs.gst_all_1.gst-plugins-libav
-];
+#  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
+#  pkgs.gst_all_1.gst-plugins-good
+#  pkgs.gst_all_1.gst-plugins-bad
+#  pkgs.gst_all_1.gst-plugins-ugly
+#  #pkgs.gst_all_1.gst-plugins-libav
+#];
+
+
+
+environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
+  gst-plugins-good
+  gst-plugins-bad
+  gst-plugins-ugly
+  gst-libav
+]);
 
 }
