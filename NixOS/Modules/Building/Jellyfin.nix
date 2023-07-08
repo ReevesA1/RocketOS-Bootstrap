3e3cd5167357723b13f7b3e09010309433c3b6cf
ReevesA1 <adamrivard87@gmail.com>
2023-07-08 07:52:18 -0400

    s

---
+++ NixOS/Modules/Building/Jellyfin.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, lib, ... }:
+
+{
+  networking.firewall = {
+    allowedTCPPorts = [
+      8096 8920 # Web frontend
+    ];
+    allowedUDPPorts = [
+      1900 7359 # Discovery
+    ];
+  };
+
+  services.jellyfin =
+  {
+    enable = true;
+    package = pkgs.jellyfin; # Upgrade to 10.6.x
+  };
+}
