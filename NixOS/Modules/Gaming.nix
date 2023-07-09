926606efd37d3c948327d97eda8e73aeb4ebd6c7
ReevesA1 <adamrivard87@gmail.com>
2023-07-09 11:12:55 -0400

    s

--- NixOS/Modules/Gaming.nix
+++ NixOS/Modules/Gaming.nix
@@ -6,10 +6,10 @@
     steam-run
     #heroic #?i dont use epic so dont bother installing it for now
     #bottles #!Giving me Issues but I think its cause I deleted it after it was build worth trying again if not use the flatpak
-    #protontricks
-    #protonup-qt
-    #playonlinux #found this have not tested it
-    #lutris
+    #protontricks #?dont know how to use
+    #protonup-qt #?dont know how to use
+    #playonlinux #?dont know how to use
+    #lutris #? Tried once did not work could not find nvidia drivers so trying to use bottles which looks easier atleast to run Diablos 4
     #(lutris.override {
     #  extraPkgs = pkgs: [
     #    wineWowPackages.stable
